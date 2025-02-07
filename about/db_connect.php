77e5c11dec44b858bcd7f53bad0a4f844e8813f0
Srinu090 <n210215@rguktn.ac.in>
2025-02-07 03:25:41 -0800

    Modified the database connection file

--- about/db_connect.php
+++ about/db_connect.php
@@ -8,7 +8,7 @@ $dbname = "library_seat_reservation";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 
-// Check connection
+// Check connection with the session
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
